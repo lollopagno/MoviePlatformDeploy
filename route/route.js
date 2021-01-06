1d2df2d65c942896d2d7856903765a61faff8b09
lollo_pagno <lorenzo.pagnini@yahoo.it>
2021-01-06 09:51:59 +0100

    Changed path to save image

--- route/route.js
+++ route/route.js
@@ -18,7 +18,7 @@ const router = express.Router();
 const multer = require("multer");
 let storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, __dirname + '/images/')
+        cb(null, '/app/images/')
     },
     filename: function (req, file, cb) {
         cb(null, file.fieldname + '-' + Date.now())
